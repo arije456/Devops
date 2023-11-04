403103734b151a8b48fdf506583d5b07390bff1b
arije456 <arije.grissiaa@esprit.tn>
2023-11-04 12:16:19 +0100

    new feature by bhm

--- src/main/java/tn/esprit/spring/TimesheetDevopsApplication.java
+++ src/main/java/tn/esprit/spring/TimesheetDevopsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TimesheetDevopsApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
 		SpringApplication.run(TimesheetDevopsApplication.class, args);
 	}
 
